ededbe1ee5593954dda1b6d72a4c0a74ab673ec8
Ansh Jindal <jindalansh498@gmail.com>
2024-09-14 19:43:34 -0400

    fixed cors error

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 
+// Use http://localhost:3000 local environment
+// Use http://dev.wouessi.com for dev envrionment
+// Use http://wouessi.com for prod environment
+
 app.use(cors({
-  origin: 'https://dev.wouessi.com' 
+  origin: 'http://dev.wouessi.com' 
 }));
 
 
