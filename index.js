46fac82013afb359b4d0d7f2cd11bffcee1b012f
Ansh Jindal <jindalansh498@gmail.com>
2024-09-14 22:18:39 -0400

    Added MongoDb again

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const express = require('express');
-const cors = require('cors');  // Import the CORS middleware
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();  // Load environment variables
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Use CORS middleware to allow requests from your frontend
+const allowedOrigin = process.env.WOUESSI_FRONTEND_URL; // Get the allowed origin from environment variables
+
 app.use(cors({
-  origin: 'https://dev.wouessi.com' // Allow requests from this domain
+  origin: allowedOrigin  // Dynamically set the allowed CORS origin
 }));
 
 // Middleware for parsing JSON
 app.use(express.json());
 
+// Connect to MongoDB using Mongoose
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('MongoDB connected!');
+}).catch((error) => {
+  console.error('MongoDB connection error:', error);
+});
+
 // Define your routes
 app.get('/', (req, res) => {
   res.send('Welcome to Wouessi Back Office');
 });
-
 
 app.get('/data', (req, res) => {
   res.json({ message: 'Hello from the server!' });
